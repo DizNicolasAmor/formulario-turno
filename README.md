5b9d80669b2df4306073746845ce77635cb90257
DizNicolasAmor <diz.nicolasamor@gmail.com>
2017-12-01 18:36:16 -0200

    version 2.0

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 Nico Diz
 
-https://github.com/DizNicolasAmor 
+https://github.com/DizNicolasAmor
 
-You can see this project here: 
-- https://diznicolasamor.github.io/formulario-turno/ 
+You can see this project here:
 
-Be free to use it and change it. 
+* https://diznicolasamor.github.io/formulario-turno/
+
+Be free to use it and change it.
 
 ![formularioREADME-1.png](/images/formularioREADME-1.png?raw=true)
-
-![formularioREADME-1.png](/images/formularioREADME-2.png?raw=true) 
--- js/index.js
+++ js/index.js
@@ -1,111 +1,124 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //calendar
+  $("#calendario").datepicker({
+    dateFormat: "dd-mm-yy",
 
-  $('#calendario').datepicker({
-    
-    dateFormat: 'dd-mm-yy',
-    
-    minDate: 0,  //calendar starts from today
-    maxDate: '8m',  //calendar ends in 8 months
-    
-    beforeShowDay: function(date){
-      
-      let day = date.getDay(),  //days from 0 to 6
-          
-          year = date.getUTCFullYear(), 
-          month = date.getUTCMonth() + 1, //months from 1-12
-          dayNumber = date.getUTCDate();
-      
+    minDate: 0, //calendar starts from today
+    maxDate: "8m", //calendar ends in 12 months
+
+    beforeShowDay: function(date) {
+      let day = date.getDay(), //days from 0 to 6
+        year = date.getUTCFullYear(),
+        month = date.getUTCMonth() + 1, //months from 1-12
+        dayNumber = date.getUTCDate();
+
       //disable hollidays in 2018     --feriados argentinos 2018
-      if(year === 2018){
-        if( (month === 1 && dayNumber === 1) ||        //año nuevo
-           (month === 2 && (dayNumber === 12 || dayNumber === 13) ) ||  //carnaval
-           (month === 3 && dayNumber === 24) ||       //día de la memoria
-           (month === 3 && dayNumber === 30) ||       //viernes santo
-           (month === 4 && dayNumber === 2)  ||       //veteranos malvinas
-           (month === 5 && dayNumber === 1)  ||       //trabajador
-           (month === 5 && dayNumber === 25) ||       //revolución de mayo
-           (month === 6 && (dayNumber === 17 || dayNumber === 20) ) ||  //fallecimiento de guemez | bandera
-           (month === 7 && dayNumber === 9)  ||       //independencia
-           (month === 8 && dayNumber === 20) ||       //fallecimiento de san martin
-           (month === 10 && dayNumber === 15)||       //diversidad cultural
-           (month === 11 && dayNumber === 19)||       //soberanía nacional
-           (month === 12 && (dayNumber === 8 || dayNumber === 25) )  //inmaculada concepción | navidad
-          ){
+      if (year === 2018) {
+        if (
+          (month === 1 && dayNumber === 1) || //año nuevo
+          (month === 2 && (dayNumber === 12 || dayNumber === 13)) || //carnaval
+          (month === 3 && dayNumber === 24) || //día de la memoria
+          (month === 3 && dayNumber === 30) || //viernes santo
+          (month === 4 && dayNumber === 2) || //veteranos malvinas
+          (month === 5 && dayNumber === 1) || //trabajador
+          (month === 5 && dayNumber === 25) || //revolución de mayo
+          (month === 6 && (dayNumber === 17 || dayNumber === 20)) || //fallecimiento de guemez | bandera
+          (month === 7 && dayNumber === 9) || //independencia
+          (month === 8 && dayNumber === 20) || //fallecimiento de san martin
+          (month === 10 && dayNumber === 15) || //diversidad cultural
+          (month === 11 && dayNumber === 19) || //soberanía nacional
+          (month === 12 && (dayNumber === 8 || dayNumber === 25)) //inmaculada concepción | navidad
+        ) {
           return [false];
         }
       }
-      
-      if(day === 0 || day === 6)  return [false];  //disable weekends
-      else  return [true];
+
+      if (day === 0 || day === 6)
+        return [false]; //disable weekends
+      else return [true];
     },
-    
+
     //spanish
-    dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
-    monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-    
+    dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+    monthNames: [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre"
+    ]
   });
-  
 
   //confirmation (modal when submit)
-  $('form').submit(function(e){
+  $("form").submit(function(e) {
     e.preventDefault();
 
     //set inputs in values
-    let dni = $('#dni').val(),
-        tel = $('#tel').val(),
-        email = $('#email').val(),
-        
-        apellido = $('#apellido').val(),
-        nombre = $('#nombre').val(),
-        sexo = $('input:checked').val(),
-        
-        fecha = $('#calendario').val(),
-        horario = $('#horario').val(),
+    let dni = $("#dni").val(),
+      tel = $("#tel").val(),
+      email = $("#email").val(),
+      apellido = $("#apellido").val(),
+      nombre = $("#nombre").val(),
+      sexo = $("input:checked").val(),
+      fecha = $("#calendario").val(),
+      horario = $("#horario").val(),
+      motivo = "",
+      comentarios = $("#comentarios").val();
 
-        motivo = '',
-        comentarios = $('#comentarios').val();
-
-    if( $('#alcoholismo').is(":checked") )  motivo = motivo + ' Alcoholismo.';
-    if( $('#drogadiccion').is(":checked") )  motivo = motivo + ' Drogadicción.';
-    if( $('#crisis').is(":checked") )  motivo = motivo + ' Crisis existencial.';
-    if( $('#otra').is(":checked") )  motivo = motivo + ' Otra patología.';
-
+    if ($("#alcoholismo").is(":checked")) motivo = motivo + " Alcoholismo.";
+    if ($("#drogadiccion").is(":checked")) motivo = motivo + " Drogadicción.";
+    if ($("#crisis").is(":checked")) motivo = motivo + " Crisis existencial.";
+    if ($("#otra").is(":checked")) motivo = motivo + " Otra patología.";
 
     //insert values in modal
-    $('#modalApellidoYNombre').text(apellido + ', ' + nombre);
-    $('#modalDNI').text(dni);
-    $('#modalEmail').text(email);
-    $('#modalFecha').text(fecha);
-    $('#modalHorario').text(horario);
-    $('#modalMotivo').text(motivo);
+    $(".modalApellidoYNombre").text(apellido + ", " + nombre);
+    $(".modalDNI").text(dni);
+    $(".modalEmail").text(email);
+    $(".modalFecha").text(fecha);
+    $(".modalHorario").text(horario);
+    $(".modalMotivo").text(motivo);
 
+    $("#modalConfirmar").show("slow");
 
-    $('#modalConfirmar').show('slow');
-
-    $('#confirmar').click(function(e){
+    //buttons
+    $("#cancelar").click(function(e) {
       e.preventDefault();
-      $('#modalConfirmar').hide('slow');
-
-      //reset values
-      $('#dni').val('');
-      $('#tel').val('');
-      $('#email').val('');
-      $('#apellido').val(''),
-      $('#nombre').val(''),
-      $('input:checked').val(''),
-      $('#calendario').val(''),
-      $('#horario').val(''),
-      motivo = '';
-      $('#comentarios').val('');
-
-      alert('Turno confirmado.\nID: 123456789');
-
+      $("#modalConfirmar").hide("slow");
     });
 
-    $('#cancelar').click(function(e){
+    $("#confirmar").click(function(e) {
       e.preventDefault();
-      $('#modalConfirmar').hide('slow');
+      $("#modalConfirmar").hide("slow");
+
+      //generate random ID
+      let randomID = parseInt(Math.random() * 800000 + 100000);
+      //insert value in html
+      $("#idTurno").text(randomID);
+
+      $("#modalTurnoID").show("slow");
+
+      $("#cerrarTurnoID").click(function() {
+        //reset values
+        $("#dni").val("");
+        $("#tel").val("");
+        $("#email").val("");
+        $("#apellido").val(""),
+          $("#nombre").val(""),
+          $("input:checked").val(""),
+          $("#calendario").val(""),
+          $("#horario").val(""),
+          (motivo = "");
+        $("#comentarios").val("");
+
+        $("#modalTurnoID").hide("slow");
+      });
     });
   });
-
 });
